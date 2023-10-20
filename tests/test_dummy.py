5b600f25b81d1eb8eabb4fabb1a9a16811c5e740
jborbely <joe.borbely@gmail.com>
2023-10-20 21:36:21 +1300

    fix elif

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -16,7 +16,7 @@ def ipv4_addresses_2():
     if sys.platform == 'linux':
         out = subprocess.check_output(['hostname', '--all-ip-addresses'])
         addresses = set(out.decode().split())
-    if sys.platform == 'darwin':
+    elif sys.platform == 'darwin':
         interfaces = socket.getaddrinfo(socket.gethostbyname(''), None, socket.AF_INET)
         addresses = set(ip[-1][0] for ip in interfaces)
     else:
