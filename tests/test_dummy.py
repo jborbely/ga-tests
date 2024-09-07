098472e9e19bd35a0eb08f4c21b36b10007fecc1
jborbely <joe.borbely@gmail.com>
2024-09-07 15:44:51 +1200

    create ctypes test

---
+++ tests/test_dummy.py
@@ -0,0 +1,16 @@
+from ctypes import *
+
+def test_roszman1():
+
+    lib = CDLL('tests/user_defined/Roszman1_x64.so')
+
+    buffer = create_string_buffer(256)
+    lib.GetFunctionName(buffer)
+    assert buffer.value == "f1: Roszman1 f1=a1-a2*x-arctan(a3/(x-a4))/pi"
+
+    num = c_int()
+    lib.GetNumParameters(byref(num))
+    assert num.value == 3
+
+    lib.GetNumVariables(byref(num))
+    assert num.value == 2
