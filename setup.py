0d0bbbf580e0ecc65a03d41c5bf812469bef8f59
jborbely <joe.borbely@gmail.com>
2025-05-03 18:14:12 +1200

    ignore tests/ and run_test.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 install_requires = []
 
-tests_require = ['pytest', 'pytest-cov']
+tests_require = ["pytest", "pytest-cov"]
 
 setup(
-    name='ga_tests',
-    version='0.0.1',
-    description='Test GitHub Actions',
-    platforms='any',
-    license='MIT',
+    name="ga_tests",
+    version="0.0.1",
+    description="Test GitHub Actions",
+    platforms="any",
+    license="MIT",
     tests_require=tests_require,
     install_requires=install_requires,
-    extras_require={'tests': tests_require},
+    extras_require={"tests": tests_require},
 )
